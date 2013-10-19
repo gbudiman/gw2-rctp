cf2de411be5666519d64ed88513d99a8e037be62
Gloria Budiman <wahyu.g@gmail.com>
2013-10-18 23:04:40 -0400

    Rails base directory commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gw2Rctp::Application.config.secret_key_base = '64beb910a275e20fa9a513bc90d6782538f68b781d5b605bf9190f8a8e241889657901813aa9da9ce3af274ba72177fe2669ae3143d17ed6d1e27d2a29ffa2fa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gw2Rctp::Application.config.session_store :cookie_store, key: '_gw2-rctp_session'
