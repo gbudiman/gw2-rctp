851cdfe4bc4e989b2c3b713cf109a76958fd6852
Gloria Budiman <wahyu.g@gmail.com>
2013-10-21 01:31:49 -0400

    Table prefill for item types completed. Fixed item type enumeration.

--- backend/RecipeBuilder.rb
+++ backend/RecipeBuilder.rb
@@ -33,30 +33,30 @@ class RecipeBuilder
 	private
 		def enumerate_type _data
 			case _data['Type']
-			when 1 then return 0			# trophy = 0					
-			when 5 then return 1			# gizmo
-			when 7 then return 2			# crafting material
-			when 8 then return 3			# container
-			when 9 then return 4			# upgrade component
-			when 10 then return 5 		# minipet
-			when 11	then return 6		# bag
-			when 12	then return 7		# back
-			when 13	then return 8		# trait guide
-			when 15	then return 9		# tool
-			when 16 then return 10		# minideck
-			when 2					# weapon = 100 - 199
-				return 100 + _data['WeaponType'] - 1
-			when 3					# armor = 200 - 299
+			when 1 then return 1			# trophy = 0
+			when 5 then return 2			# gizmo
+			when 7 then return 3			# crafting material
+			when 8 then return 4			# container
+			when 9 then return 5			# upgrade component
+			when 10 then return 6 			# minipet
+			when 11	then return 7			# bag
+			when 12	then return 8			# back
+			when 13	then return 9			# trait guide
+			when 15	then return 10			# tool
+			when 16 then return 11			# minideck
+			when 2							# weapon = 100 - 199
+				return 100 + _data['WeaponType']
+			when 3							# armor = 200 - 299
 				return 200 +
-					10 * _data['ArmorType'] - 1 + 
-					_data['ArmorWeightType'] - 1
-			when 4					# consumable = 800 - 899
-				return 800 + _data['ConsumableType'] - 1
-			when 6					# trinket = 900 - 909
-				return 900 + _data['TrinketType'] - 1
-			when 14					# gathering = 910 - 919
+					10 * _data['ArmorType'] + 
+					_data['ArmorWeightType']
+			when 4							# consumable = 800 - 899
+				return 800 + _data['ConsumableType']
+			when 6							# trinket = 900 - 909
+				return 900 + _data['TrinketType']
+			when 14							# gathering = 910 - 919
 				return 910 + _data['GatheringType']
-			else return 999			# unspecified type
+			else return 999					# unspecified type
 			end
 		end
 end
